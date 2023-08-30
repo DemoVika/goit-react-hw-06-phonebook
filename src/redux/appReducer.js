ba17d05d48ebeba0728e097f2760bde0dc18a8cb
Viktoria <viktoria.ttravel@gmail.com>
2023-08-30 15:23:53 +0300

    update

--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  contacts: JSON.parse(localStorage.getItem('contacts') || []),
+  contacts: [],
   filter: '',
 };
 
@@ -11,19 +11,13 @@ const appSlice = createSlice({
   reducers: {
     // Объект редюсеров
     addContact(state, action) {
-      // if (state.contacts.find(item => item.name === action.payload.name)) {
-      //   alert(`${action.payload} is already in contacts`);
-      //   return;
-      // }
       state.contacts.push(action.payload);
-      localStorage.setItem('contacts', JSON.stringify(state.contacts));
     },
     deleteContact(state, action) {
       const index = state.contacts.findIndex(
         item => item.id === action.payload
       );
       state.contacts.splice(index, 1);
-      localStorage.setItem('contacts', JSON.stringify(state.contacts));
     },
     setFilter(state, action) {
       state.filter = action.payload;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { appReducer } from './appReducer';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // используем localStorage
-
-// import rootReducer from './reducers'; // ваш корневой редьюсер
+import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 
 const persistConfig = {
-  key: 'root', // ключ, по которому будут сохранены данные
-  storage, // используем localStorage
+  key: 'root',
+  storage,
   blacklist: ['filter'],
 };
 
-const rootReducer = combineReducers({
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
   reducer: {
     app: appReducer,
   },
 });
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore(persistedReducer);
 export const persistor = persistStore(store);
-
-// export const store = configureStore({
-//   reducer: {
-//     app: appReducer,
-//   },
-// });
